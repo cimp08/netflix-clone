3a92decdc2abf6c3e8d54a5c25dd94825bbe481b
Cemil Ülay <cemilulay@gmail.com>
2022-10-08 12:28:33 +0200

    add plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [
+    require("tailwind-scrollbar-hide"),
+    plugin(function ({ addVariant }) {
+      addVariant("not-last", "&:not(:last-child)");
+      addVariant("not-first", "&:not(:first-child)");
+    }),
+  ],
 };
