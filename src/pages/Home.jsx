54793c2cd5fbd1b16c7d875598b4ee1071d6d097
Cemil Ülay <cemilulay@gmail.com>
2022-10-08 12:29:11 +0200

    add slider, delete main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Main from '../components/Main'
+import React from "react";
+import Slider from "../components/Slider";
+import Row from "../components/Row";
+import requests from "../Requests";
 
 const Home = () => {
   return (
     <>
-      <Main />
+      <Slider />
+      <Row rowID="1" title="Coming Soon" fetchURL={requests.requestUpcoming} />
+      <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowID="3" title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowID="4" title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowID="5" title="Horror" fetchURL={requests.requestPopular} />
     </>
   );
-}
-
-export default Home
+};
+export default Home;
