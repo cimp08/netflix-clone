29ec161c4a1d59f3413108458d1d902cc42d77b1
Cemil Ülay <cemilulay@gmail.com>
2022-10-09 11:44:53 +0200

    create Movie page

---
+++ src/pages/Movie.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MediaHero from "../components/movies/MediaHero";
+
+const Movie = () => {
+  return (
+    <>
+      <MediaHero />
+    </>
+  );
+}
+
+export default Movie
