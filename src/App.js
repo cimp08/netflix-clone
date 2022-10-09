a7955446162d19602de798dda488999e45426212
Cemil Ülay <cemilulay@gmail.com>
2022-10-09 11:41:18 +0200

    update start path and add movieDetail

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthContextProvider } from "./context/AuthContext";
 import Account from "./pages/Account";
 import Home from "./pages/Home";
+import Movie from "./pages/Movie"
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
@@ -13,7 +14,10 @@ function App() {
       <AuthContextProvider>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* Setting home path to /movies/popular because of styling on the active link */}
+          <Route path="/" element={<Navigate to="/movies" />} />
+          <Route path="/movies" element={<Home />} />
+          <Route path="/movies/:movieId" element={<Movie />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route
